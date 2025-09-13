3687d8f564941ff7b371fb8d00e274bd819aa86b
nhan124abc <caonhan15a@gmail.com>
2025-09-13 21:33:57 +0700

    okk

--- react-fontend/app/src/App.js
+++ react-fontend/app/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import EmployeeList from './EmployeeList';
-
+import EmployeeList from './component/EmployeeList';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CreateEmployee from "./component/CreateEmployee";
 function App() {
   return (
-    <div className="App">
-      <EmployeeList />
-    </div>
+      <Router>
+
+          <div className="App">
+              <nav className="navbar">
+                  <a href="/">Danh Sách Nhân Viên</a>
+                  <a href="/add">Thêm Nhân Viên</a>
+              </nav>
+              <Routes>
+                  <Route path="/" element={<EmployeeList/>}/>
+                  <Route path="/add" element={<CreateEmployee/>}/>
+              </Routes>
+          </div>
+
+      </Router>
+
   );
 }
 
---
+++ react-fontend/app/src/component/CreateEmployee.js
@@ -0,0 +1,48 @@
+import React,{useState,useEffect} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default function CreateEmployee(){
+    const [id,setId]=useState("")
+    const [name,setName]=useState("")
+    const [email,setEmail]=useState("")
+    function HandleSave(){
+
+    }
+    const addemployee= async (e)=>{
+
+        const employeedata={name,email}
+        try{
+            const response=await fetch("http://localhost:8080/api/employees/add",
+                {
+                    method:"POST",
+                    headers:{"Content-Type":"application/Json"},
+                    body:JSON.stringify(employeedata)
+
+                })
+        }catch (error){
+
+        }
+    }
+    function HandleCancel(){
+        setId("");
+        setEmail("")
+        setName("")
+    }
+    return(
+        <>
+            <div className='container mb-5 '>
+                <h1 className='text-center'>Add Employee</h1>
+            <form onSubmit={addemployee} >
+
+                <input value={name} onChange={(e) => setName(e.target.value)} required/>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} required/>
+
+                <button type='submit'> Add Employee</button>
+
+
+            </form>
+            </div>
+        </>
+    )
+
+}
