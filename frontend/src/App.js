26cb4a64d2278ca1ce9b3020de43d4e8ceb5e85e
nhan124abc <caonhan15a@gmail.com>
2025-09-26 23:19:23 +0700

    sua loi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import EmployeeList from "./EmployeeList";
-import CreateEmployee from "./CreateEmployee";
-import NotFound from "./NotFound";
+import EmployeeList from "./component/EmployeeList";
+import CreateEmployee from "./component/CreateEmployee";
+import NotFound from "./component/NotFound";
 
 function App() {
   return (
     <Router>
-         <div className="App">
-        <nav className="navbar">
-          <Link to="/">Danh Sách Nhân Viên</Link>
-          <Link to="/add">Thêm Nhân Viên</Link>
-        </nav>
       <Routes>
         <Route path="/" element={<EmployeeList />} />
         <Route path="/add" element={<CreateEmployee />} />
         <Route path="*" element={<NotFound />} /> 
       </Routes>
-      </div>
     </Router>
   );
 }
